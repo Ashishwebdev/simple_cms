a900ae8bf6eb351d6e413a0c92d4412a8e06f9a5
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-03-24 22:14:35 +0530

    new record can be added and shown to the index

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -1,26 +1,38 @@
 class TestingController < ApplicationController
   def index
- 
      @testings = Testing.sorted
      render ('index')
-
   end
 
   def show
+    @testing=Testing.find(params[:id])
   end
 
   def new
+    @testing = Testing.new
   end
   def create
+    @testing = Testing.new(params.require(:testing).permit(:name,:position,:visible))
+     if @testing.save
+      redirect_to ({:action=> 'index',:id=> @testing.id})
+     end
+
+   else
+    return ('new')
+   end
+
   end
 
   def edit
   end
+
   def update
   end
 
   def delete
   end
+
   def destroy
   end
-end
+
+
